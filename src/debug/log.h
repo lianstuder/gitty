3d7a30ca9d5cdbaba93a212bdc290b88473cb9e3
fastuder <lian.studer@hslu.ch>
2021-06-24 17:29:39 +0200

    feat: logging system

--- src/debug/log.h
+++ src/debug/log.h
@@ -1,4 +1,7 @@
 #pragma once
-#include <iostream>
+#include <color.h>
 
-#define GT_LOG_WARNING(logMsg) std::cout << "WARNING: " << logMsg << std::endl;
+#define GT_LOG_INFO(logMsg) std::cout << dye::green("INFO: " logMsg) << std::endl;
+#define GT_LOG_WARNING(logMsg) std::cout << dye::yellow("WARNING: " logMsg) << std::endl;
+#define GT_LOG_ERROR(logMsg) std::cout << dye::red("ERROR: " logMsg) << std::endl;
+#define GT_LOG_FATAL(logMsg) std::cout << dye::white_on_red("FATAL: " logMsg) << std::endl;
